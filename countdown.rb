614ec3f2dea64d7742f7c982f0b26aea00311bba
Bryna Dunnells <badunnells@gmail.com>
2019-05-02 17:00:26 +0000

    sleepy

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(s)
+  while s > 0
+    puts "#{s} SECOND(S)!"
+      s -= 1
+    end
+    return "HAPPY NEW YEAR!"
+
+end
+def countdown_with_sleep(s)
+  while s > 0
+    puts "#{s} SECOND(S)!"
+      s -= 1
+      sleep(1)
+    end
+    return "HAPPY NEW YEAR!"
+
 end
