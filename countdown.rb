9357337e7ea58f3593f17f0d91074b38c2e1b6a6
Danielle Martinoli <daniechris5@gmail.com>
2019-05-03 22:46:51 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -8,14 +8,10 @@ end
 
 def countdown_with_sleep(num_secs)
   num_secs = 0
-<<<<<<< HEAD
 while num_secs > 0
-=======
-while counter < num_secs
->>>>>>> 118bd8c97ce5dc2ea047186f4d041140e8005f76
   puts "#{num_secs} SECOUND(S)!"
   num_secs -= 1
   sleep 5
   end
- "HAPPY NEW YEAR!"
+    "HAPPY NEW YEAR!"
 end
