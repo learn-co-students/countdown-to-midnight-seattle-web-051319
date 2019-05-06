a6be2967f1f84d10c0be3290c06259319613c18c
AidiRi <rivera.aidi@gmail.com>
2019-05-06 16:09:43 -0700

    Countdown to Midnight

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+def countdown(x)
+	num_seconds = x
+	while num_seconds > 0
+		puts "#{num_seconds} SECOND(S)!"
+		num_seconds -= 1
+	end
+	"HAPPY NEW YEAR!"
+end
+
+
+
+def countdown_with_sleep(x)
+	num_seconds = x
+	while num_seconds > 0
+		puts "#{num_seconds} SECOND(S)!"
+		sleep 1
+		num_seconds -= 1
+	end
+	"HAPPY NEW YEAR!"
 end
