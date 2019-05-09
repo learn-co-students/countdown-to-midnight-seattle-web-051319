d00429adfd4e550f6d711e318ec31de23332a061
bustling-maker-4835 <chopra.chet@gmail.com>
2019-05-09 00:03:38 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(num)
+
+  num.times do |i|
+    puts "#{num - i} SECOND(S)!"
+    
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num)
+  sleep(5)
+  num.times do |i|
+    puts "#{num - i} SECOND(S)!"
+    
+  end
+  return "HAPPY NEW YEAR!"
 end
